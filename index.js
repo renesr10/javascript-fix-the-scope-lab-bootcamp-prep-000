2c1d5d630d552fe144f40e86a491c2fd28fd114b
Rene Santana <renesantana10@gmail.com>
2018-07-05 22:06:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
    var animal = yourAnimal
-   return animal
+   return animal()
 }
 
 function add2(n) {
